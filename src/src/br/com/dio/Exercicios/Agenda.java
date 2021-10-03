c12faa8e73302b38d9fd5b730e5e085238863fc9
mojisilva <mojiasilva@gmail.com>
2021-10-03 19:16:15 -0300

    Inclui exercício "Agenda"
    Este commit se refere à inclusão do exercício "Agenda" que consiste em um código que tem a função de listar o conteúdo de um array de uma agenda telefônica.

---
+++ src/src/br/com/dio/Exercicios/Agenda.java
@@ -0,0 +1,13 @@
+package br.com.dio.Exercicios;
+
+public class Agenda {
+    public static void main(String[] args) {
+        String[][] agenda = {{"João","São Paulo","(11) 9999-5241"},{"Maria","Ribeirão Preto","(16) 9999-8596"},{"Ana","Manaus","(92) 9999-8574"}};
+        for(int i=0; i< agenda.length;i++){
+            System.out.println("----------------------------");
+            for(int x = 0;x< agenda.length;x++){
+                System.out.println(agenda[i][x]);
+            }
+        }
+    }
+}
