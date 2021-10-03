2baa3967f6de1a652c5e277bd6768f76d52aa62b
mojisilva <mojiasilva@gmail.com>
2021-10-03 19:02:46 -0300

    Inclui exercício "Frutas"
    Este commit se refere à inclusão do exercício "Frutas" cujo código consiste em escrever em tela o conteúdo do vetor frutas, enquanto o contador foi igual a quantidade de posições do vetor

---
+++ src/src/br/com/dio/Exercicios/Frutas.java
@@ -0,0 +1,14 @@
+package br.com.dio.Exercicios;
+
+public class Frutas {
+    public static void main(String[] args) {
+        String[] fruta = new String[4];
+        fruta[0] = "Pera";
+        fruta[1] = "Maçã";
+        fruta[2] = "Manga";
+        fruta[3] = "Uva";
+        for(int i=0; i<fruta.length;i++){
+            System.out.println(fruta[i]);
+        }
+    }
+}
