38b2da94d32cdd6c20c96595d939a56f6afb6225
mojisilva <mojiasilva@gmail.com>
2021-10-02 23:33:45 -0300

    Inclui exercício "CestaFrutas"
    Este commit se refere à inclusão do exercício "CestaFrutas" cujo código consiste em um array chamado cesta que deve ser percorrido enquanto o contador for menor ou igual à quantidade de posições do array.

---
+++ src/src/br/com/dio/Exercicios/CestaFrutas.java
@@ -0,0 +1,11 @@
+package br.com.dio.Exercicios;
+
+public class CestaFrutas {
+    public static void main(String[] args) {
+        String [][] cesta = {{"Maça", "100"}, {"Pera", "200"}, {"Uva", "300"}, {"Java", "400"}};
+        for(int i=0; i<cesta.length; i++){
+            System.out.println("Produto: " + cesta[i][0] + " - Quantidade: " + cesta[i][0]);
+        }
+
+    }
+}
