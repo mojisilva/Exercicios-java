7b86fb52cf6d9984ad54ca7eb987f669f5616787
mojisilva <mojiasilva@gmail.com>
2021-10-03 10:45:33 -0300

    Exercício Inclui "Equacoes"
    Este commit se refere à inclusão do exercício "Equacoes" que tem por objetivo informar o valor de quatro equações previamente descritas no código.

---
+++ src/src/br/com/dio/Exercicios/Equacoes.java
@@ -0,0 +1,14 @@
+package br.com.dio.Exercicios;
+
+public class Equacoes {
+    public static void main(String[] args) {
+        double equa1 = (3-2-1+1+2+3);
+        double equa2 = (2*3-4*5);
+        double equa3 = (2+6-3/7*9);
+        double equa4 = (3%4-8);
+        System.out.println("O valor da primeira equação é: " + equa1);
+        System.out.println("O valor da segunda equação é: " + equa2);
+        System.out.println("O valor da terceira equação é: " + equa3);
+        System.out.println("O valor da quarta equação é: " + equa4);
+    }
+}
