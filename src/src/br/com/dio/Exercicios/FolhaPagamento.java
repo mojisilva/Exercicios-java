4ec697d176674d0229a43f032725d5354cb54993
mojisilva <mojiasilva@gmail.com>
2021-10-03 18:58:52 -0300

    Inclui exercício "FolhaPagamento"
    Este commit se refere à inclusão do exercício "FolhaPagamento" que consiste em um código que deve solicitar ao usuário o código de um colaborador da empresa, o número de horas trabalhadas e o valor do salário hora do colaborador, o programa deve retornar o código do trabalhador e o salário total.

---
+++ src/src/br/com/dio/Exercicios/FolhaPagamento.java
@@ -0,0 +1,27 @@
+package br.com.dio.Exercicios;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class FolhaPagamento {
+
+    public static void main(String[] args) throws IOException {
+        InputStream in;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Digite o código do colaborador");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int valor1 = Integer.parseInt(st.nextToken());
+        System.out.println("Digita a quantidade de horas trabalhadas do colaborador");
+        st = new StringTokenizer(br.readLine());
+        int valor2 = Integer.parseInt(st.nextToken());
+        System.out.println("Digite o valor do salário hora do colaborador");
+        st = new StringTokenizer(br.readLine());
+        double valor3 = Double.parseDouble(st.nextToken());
+        double salario = valor2*valor3;
+        System.out.println("Código do colaborador = " + valor1);
+        System.out.printf("Salário = R$ %.2f ", salario);
+    }
+}
