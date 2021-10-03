2165c2e9225e78b3870b5a36fb609975455117c2
mojisilva <mojiasilva@gmail.com>
2021-10-03 00:08:16 -0300

    Inclui Exercício "Crime"
    Este commit se refere à inclusão do exercício "Crime" que tem por função criar uma lista que armazanará a respostas de 5 perguntas sobre um crime e definir se a pessoa é inocente, suspeita, cúmplice ou assassina, a partir da quantidade de respostas positivas.

---
+++ src/src/br/com/dio/Exercicios/Crime.java
@@ -0,0 +1,59 @@
+package br.com.dio.Exercicios;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Crime {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        String r;
+        int cont=0;
+        ArrayList <String>  respostas = new ArrayList<>();
+        System.out.println("Responda S ou N para as perguntas a seguir");
+        System.out.println("Telefonou para a vítima?");
+        r = ler.nextLine();
+        r = r.toUpperCase();
+        respostas.add(r);
+        System.out.println("Esteve no local do crime?");
+        r = ler.nextLine();
+        r = r.toUpperCase();
+        respostas.add(r);
+        System.out.println("Mora perto da vítima?");
+        r = ler.nextLine();
+        r = r.toUpperCase();
+        respostas.add(r);
+        System.out.println("Devia para a vítima?");
+        r = ler.nextLine();
+        r = r.toUpperCase();
+        respostas.add(r);
+        System.out.println("Já trabalhou com a vítima?");
+        r = ler.nextLine();
+        r = r.toUpperCase();
+        respostas.add(r);
+        System.out.println(respostas);
+        Iterator<String> i = respostas.iterator();
+        while(i.hasNext()){
+            if(respostas.contains("S")){
+                cont++;
+                i.next();
+            }
+        }
+        System.out.println("Você respondeu a " + cont + " respostas positivas");
+        switch (cont){
+            case 2:
+                System.out.println("Você foi classificado(a) como suspeito(a)");
+            break;
+            case 3:
+            case 4:
+                System.out.println("Você foi classificado(a) como cúmplice");
+            break;
+            case 5:
+                System.out.println("Você foi classificado(a) como assassino(a)");
+            break;
+            default:
+                System.out.println("Você foi classificado(a) como inocente");
+        }
+        ler.close();
+    }
+}
