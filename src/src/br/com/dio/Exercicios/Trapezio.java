9f7cbf2072eb852287b988f975f88e3147e71f35
mojisilva <mojiasilva@gmail.com>
2021-10-05 22:35:00 -0300

    Inclui exercício "Trapezio"
    Este commit se refere à inclusão do exercício "Trapezio" que tem por objetivo calcular a área de um trapézio e depois arredondar o valor da área para um número inteiro.

---
+++ src/src/br/com/dio/Exercicios/Trapezio.java
@@ -0,0 +1,25 @@
+package br.com.dio.Exercicios;
+
+import java.util.Scanner;
+
+public class Trapezio {
+    public static void main(String[] args) {
+        double area;
+        double baseMaior;
+        double baseMenor;
+        double altura;
+        int conversor;
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Qual o tamanho da base maior do trapézio");
+        baseMaior = ler.nextDouble();
+        System.out.println("Qual o tamanho da base menor do trapézio");
+        baseMenor = ler.nextDouble();
+        System.out.println("Qual o tamanho da altura do trapézio");
+        altura = ler.nextDouble();
+        area = (altura * (baseMaior+baseMenor))/2;
+        System.out.printf("O tamanho da área do trapézio é: %.2f\n", area);
+        conversor = (int) area;
+        System.out.println("O tamanho arrendodado da área do trapézio é: " + conversor);
+        ler.close();
+    }
+}
