514e5a37f264902e8eada2e6c20412dfa982b9a7
mojisilva <mojiasilva@gmail.com>
2021-10-05 22:55:25 -0300

    Inclui exercício "VerificaNumero"
    Este commit se refere à inclusão do exercício "VerificaNumero" que consiste em um código que solicita um número de um usuário e informa se o número é primo ou não e se o número é par ou ímpar.

---
+++ src/src/br/com/dio/Exercicios/VerificaNumero.java
@@ -0,0 +1,31 @@
+package br.com.dio.Exercicios;
+
+import java.util.Scanner;
+
+public class VerificaNumero {
+    public static void main(String[] args) {
+        int numero;
+        int i=0;
+        int contador = 0;
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Digite um número");
+        numero = ler.nextInt();
+        while(numero >=i){
+            i++;
+            if(numero % i ==0){
+                contador++;
+            }
+        }
+        if((contador<=2) && (contador>1)){
+            System.out.println("O número "+  numero + " é primo");
+        }else{
+            System.out.println("O número "+  numero + " não é primo");
+        }
+        if(numero % 2 == 0){
+            System.out.println("O número " + numero + " é par");
+        }else{
+            System.out.println("O número " + numero + " é ímpar");
+        }
+        ler.close();
+    }
+}
