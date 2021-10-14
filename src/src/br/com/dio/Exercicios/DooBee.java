d70047dd86a04176ef92a23872df1c28b9501d9c
mojisilva <mojiasilva@gmail.com>
2021-10-13 21:46:32 -0300

    Inclui Exercício "DooBee"
    Este commit se refere à inclusão do exercício "DooBee" que tem por função escrever uma frase utilizando o loop while.

---
+++ src/src/br/com/dio/Exercicios/DooBee.java
@@ -0,0 +1,14 @@
+package br.com.dio.Exercicios;
+
+public class DooBee {
+    public static void main(String[] args) {
+        int x = 1;
+        while( x < 3){
+            System.out.print("Doo");
+            System.out.print("Bee");
+            x++;
+        }
+        if(x == 3)
+            System.out.print("Do");
+    }
+}
